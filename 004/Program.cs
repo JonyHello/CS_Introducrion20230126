de0afae9a1f728a82357ce556d8bd220ebf3fd90
Jony <trallipop43@gmail.com>
2023-01-29 20:04:41 +0300

    Lesson 1_03

---
+++ 004/Program.cs
@@ -0,0 +1,18 @@
+﻿//4. По заданному с клавиатуры номеру дня недели вывести его название
+int a=0;
+System.Console.Write("Введите номер дня ");
+a=Convert.ToInt32(Console.ReadLine()); 
+if (a==1)
+System.Console.WriteLine("Выбронный день - Понедельник");
+if (a==2)
+System.Console.WriteLine("Выбронный день - Вторник");
+if (a==3)
+System.Console.WriteLine("Выбронный день - Среда");
+if (a==4)
+System.Console.WriteLine("Выбронный день - Четверг");
+if (a==5)
+System.Console.WriteLine("Выбронный день - Пятница");
+if (a==6)
+System.Console.WriteLine("Выбронный день - Суббота");
+if (a==7)
+System.Console.WriteLine("Выбронный день - Воскресенье");
---
+++ 005/Program.cs
@@ -0,0 +1,21 @@
+﻿//5.С клавиатуры вводятся три числа. Найти максимальное из трех чисел
+int a,b,c;
+System.Console.Write("Введите первое число ");
+a=Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число ");
+b=Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите третье число ");
+c=Convert.ToInt32(Console.ReadLine());
+int Max;
+
+Max=a;
+
+if (b>a)
+{
+Max=b;
+}
+if (c>b)
+{
+Max=c;
+}
+System.Console.WriteLine($"Вывод {Max} ");
