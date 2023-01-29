238e978816fef9e5ee2df44c71f611a405138e9d
Jony <trallipop43@gmail.com>
2023-01-29 23:32:58 +0300

    Lesson 1_04

---
+++ 008/Program.cs
@@ -0,0 +1,11 @@
+﻿// Вывести на экран числа от -N до N
+int i=-10;
+int N=10;
+System.Console.Write("Введите число:");
+N=Convert.ToInt32(Console.ReadLine());
+
+while(i<=N)
+{
+  System.Console.WriteLine(i);
+  i=i+1;   
+}
