9dd1cd7ec66f0c8efa0ae56c6161c6baaba7900c
Jony <trallipop43@gmail.com>
2023-02-06 22:25:42 +0300

    Lesson_3__HomeWork_1

---
+++ 031/Program.cs
@@ -0,0 +1,9 @@
+﻿//31. Вывести на экран кубы чисел от 1 до N
+Random random=new Random();
+for (int i=0;i<10;i++);
+{
+    int a=random.Next(1,5);
+    int b=(int)Math.Pow(a,3);
+
+    System.Console.WriteLine($"Рандомное число {a}, Куб числа {b}");
+}
---
+++ 033/Program.cs
@@ -0,0 +1,16 @@
+﻿//33.Возведите число А в натуральную степень B используя цикл
+System.Console.Write("Введите число: ");
+int a = Int32.Parse(System.Console.ReadLine());
+System.Console.Write("Введите необходимую степень: ");
+int b = Int32.Parse(System.Console.ReadLine());
+
+double digitPow(double x, double y)
+{
+    double d = 1;
+    for(int i = 1; i <= y; i++)
+    {
+        d = d*x;
+    }
+    return d;
+}
+System.Console.WriteLine(digitPow(a,b));
