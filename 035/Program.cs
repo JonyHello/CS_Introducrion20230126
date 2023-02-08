a797e74f155b2341388086ba244fe6bfaa20d6d8
Jony <trallipop43@gmail.com>
2023-02-08 20:20:43 +0300

    Lesson_3__HomeWork_3.0

--- 035/Program.cs
+++ 035/Program.cs
@@ -1,9 +1,15 @@
 ﻿//35.Подсчитать сумму цифр в числе. Сделать подпрограмму.
 int n=123;
+
+System.Console.WriteLine($"s{SumDigiths()}");
+
+int SumDigiths(int n=123)
+{
 int s=0;
 while (n!=0)
 {
     s=s+n%10;
     n=n/10;
 }
-System.Console.WriteLine($"s{s}");
+return s;
+}
---
+++ 036/Program.cs
@@ -0,0 +1,10 @@
+﻿//36.Написать программу вычисления произведения чисел от 1 до N
+int N=int.Parse(Console.ReadLine());
+int Proizvedenie(int N)
+{
+    int S=1;
+for(int i=1;i<=N;i++)
+    S=S*i;
+    return S;
+}
+System.Console.Write($"{Proizvedenie(N)}");
