0bd7f442b70e6be9504d6172b85f5704433f0dd1
Jony <trallipop43@gmail.com>
2023-02-17 12:50:08 +0300

    Lesson_6__HomeWork_1.0

---
+++ 062/Program.cs
@@ -0,0 +1,45 @@
+﻿//62. В двумерном массиве n×k заменить четные элементы на противоположные по значению (a[i,j]=-a[i,j]). Написать отдельную подпрограмму на вход которой подается массив, а на выходе измененный массив.
+
+int[,] Random2DArray (int N,int M,int min,int max)
+{
+int [,] a = new int[N,M];
+Random random=new Random();
+for (int i=0;i<a.GetLength(0);i++)
+    for (int j=0;j<a.GetLength(0);j++)
+    a[i,j]=random.Next(min,max+1);
+    return(a);
+}
+
+void Print2DArry(int[,] a)
+{
+    for(int i=0;i<a.GetLength(0);i++)
+        {
+            for (int j=0;j<a.GetLength(1);j++)
+            System.Console.Write($"{a[i,j],5}");
+            System.Console.WriteLine();
+        }
+        {
+            System.Console.WriteLine();
+        }
+}
+int [,] a=Random2DArray(4,4,0,10);
+Print2DArry(a);
+
+
+
+int [,] Change2DArray(int[,] a)
+{
+    int[,] NewArray = new int [a.GetLength(0),a.GetLength(1)];
+    for (int i=0;i<a.GetLength(0);i++)
+    {
+        for (int j=0;j<a.GetLength(1);j++)
+        {
+            if (a[i,j]%2==0) NewArray[i,j]= -a[i,j];
+            else NewArray[i,j]= a[i,j];
+        }
+    }
+return NewArray;
+}
+
+int[,] NewArray=Change2DArray(a);
+Print2DArry(NewArray);
---
+++ 065/Program.cs
@@ -0,0 +1,60 @@
+﻿//65 Показать двумерный массив размером m×n заполненный вещественными случайными числами
+int [,] Random2Darray(int N, int M,int min,int max)
+{
+    int[,] a=new int[N,M];
+    Random random=new Random();
+    for (int i=0; i<a.GetLength(0);i++)
+        for (int j=0;j<a.GetLength(1);j++)
+        a[i,j]=random.Next(min,max+1);
+        return a;
+}
+
+void Print2DArry(int[,] a)
+{
+    for (int i=0;i<a.GetLength(0);i++)
+    {
+        for (int j=0;j<a.GetLength(1);j++)
+        System.Console.Write($"{a[i,j],5}");
+        System.Console.WriteLine();
+    
+    }
+}
+
+int[,] a=Random2Darray(3,4,0,10);
+Print2DArry(a);
+int r,c;
+
+if (FindElemenrIn2DArray(a,10,out r,out c))
+System.Console.WriteLine($"row={r} colum={c}");
+else 
+System.Console.WriteLine("Element not found");
+
+bool FindElemenrIn2DArray(int [,] a, int find, out int row,out int colum)
+{
+     row=-1;
+     colum=-1;
+    for (int i=0; i<a.GetLength(0);i++)
+        for (int j=0;j<a.GetLength(1);j++)
+        if(a[i,j]==find)
+    {
+        row=i;
+        colum=j;
+        return true;
+    }
+    return false;
+}
+
+
+//66.Написать подпрограмму, которая в квадратной матрице чисел находит сумму элементов главной диагонали
+int SumMainDiaganal (int[,] a)
+{
+int s=0;
+for (int i=0; i<a.GetLength(0);i++)
+        for (int j=0;j<a.GetLength(1);j++)
+        if (i==j)
+        s=s+a[i,j];
+        for (int i=0; i<a.GetLength(0);i++)
+        s=s+a[i,i];
+        return s;
+}
+System.Console.WriteLine(SumMainDiaganal(a));
